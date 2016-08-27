575e77487c2fdcf6baf62a8ab9999c31e91d9841
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 16:08:35 -0700

    debug

--- packages/ninjapenguin-common/lib/components/ContactUs.jsx
+++ packages/ninjapenguin-common/lib/components/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     <div>
       <h1 style="font-family:century gothic; color:#004de6;">Contact Us</h1>
       <p style= "font-family:century gothic; color:#808080;">
-        Email: support@rentech.online <br>
-        Phone: 1-800-REN-TECH (1-800-736-8324) <br>
+        Email: support@rentech.online <br/>
+        Phone: 1-800-REN-TECH (1-800-736-8324) <br/>
       </p>
     </div>
   )
