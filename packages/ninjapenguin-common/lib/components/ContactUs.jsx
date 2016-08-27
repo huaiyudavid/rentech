dc2d7103a8df3855c7f5ac3764d82b6de0ecfbdb
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 16:35:31 -0700

    debug

--- packages/ninjapenguin-common/lib/components/ContactUs.jsx
+++ packages/ninjapenguin-common/lib/components/ContactUs.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const ContactUs = () => {
+  var headerStyle = {
+    font-family:century gothic,
+    color: #004DE6
+  };
+
+  var bodyStyle = {
+    font-family:century gothic,
+    color: #808080
+  };
+
   return (
     <div>
-      <h1 style={font-family:century gothic, color:#004de6}>Contact Us</h1>
-      <p style={font-family:century gothic, color:#808080}>
+      <h1 style={headerStyle}>Contact Us</h1>
+      <p style={bodyStyle}>
         Email: support@rentech.online <br/>
         Phone: 1-800-REN-TECH (1-800-736-8324) <br/>
       </p>
