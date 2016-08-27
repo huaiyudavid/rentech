4fc8097232b0d3db1eacfdbd5903ff10fa74fd48
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 16:05:37 -0700

    fix contact us

--- packages/ninjapenguin-common/lib/components/ContactUs.jsx
+++ packages/ninjapenguin-common/lib/components/ContactUs.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 const ContactUs = () => {
   return (
     <div>
-      <h1>Welcome To My Custom Page!</h1>
       <h1 style="font-family:century gothic; color:#004de6;">Contact Us</h1>
       <p style= "font-family:century gothic; color:#808080;">
-      Email: support@rentech.online<br>
-      Phone: 1-800-REN-TECH (1-800-736-8324) <br>
-      <p>
-      <p>Nice to meet you.</p>
+        Email: support@rentech.online <br>
+        Phone: 1-800-REN-TECH (1-800-736-8324) <br>
+      </p>
     </div>
-
   )
 }
 
