5f0ffaafd23ce864f46d1cce23e27abffbc94dbf
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 16:13:27 -0700

    debug

--- packages/ninjapenguin-common/lib/components.js
+++ packages/ninjapenguin-common/lib/components.js
@@ -5,6 +5,7 @@ This file centralizes all our custom component overrides.
 import CustomLogo from "./components/CustomLogo.jsx";
 import CustomNewsletter from "./components/CustomNewsletter.jsx";
 import CustomPostsItem from "./components/CustomPostsItem.jsx";
+import CustomFooter from "./components/CustomFooter.jsx";
 
 Telescope.components.Logo = CustomLogo;
 Telescope.components.Newsletter = CustomNewsletter;
