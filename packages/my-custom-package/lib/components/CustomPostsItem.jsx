d98a381a45a6aba1dbca62acef1ae8629d11d463
suitangi <suitangi777@gmail.com>
2016-08-27 23:58:11 -0700

    Might Break 5.6

--- packages/my-custom-package/lib/components/CustomPostsItem.jsx
+++ packages/my-custom-package/lib/components/CustomPostsItem.jsx
@@ -13,7 +13,7 @@ class CustomPostsItem extends Telescope.components.PostsItem {
 
     const post = this.props.post;
 
-    let postClass = "posts-item"; 
+    let postClass = "posts-item";
     if (post.sticky) postClass += " posts-sticky";
 
     // ⭐ custom code starts here ⭐
@@ -24,22 +24,22 @@ class CustomPostsItem extends Telescope.components.PostsItem {
 
     return (
       <div className={postClass}>
-        
+
         <div className="posts-item-vote">
           <Telescope.components.Vote post={post} currentUser={this.context.currentUser}/>
         </div>
-        
+
         {post.thumbnailUrl ? <Telescope.components.PostsThumbnail post={post}/> : null}
 
         <div className="posts-item-content">
-          
+
           <h3 className="posts-item-title">
             <Link to={Posts.getLink(post)} className="posts-item-title-link" target={Posts.getLinkTarget(post)}>
-              {post.title}
+              {post.Price}{post.title}
             </Link>
             {this.renderCategories()}
           </h3>
-          
+
           <div className="posts-item-meta">
             {post.user? <div className="posts-item-user"><Telescope.components.UsersAvatar user={post.user} size="small"/><Telescope.components.UsersName user={post.user}/></div> : null}
             <div className="posts-item-date"><FormattedRelative value={post.postedAt}/></div>
@@ -55,13 +55,13 @@ class CustomPostsItem extends Telescope.components.PostsItem {
         </div>
 
         {this.renderCommenters()}
-        
-      
+
+
       </div>
     )
   }
 };
-  
+
 CustomPostsItem.propTypes = {
   post: React.PropTypes.object.isRequired
 }
