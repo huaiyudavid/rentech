afa0f606f40be5b311feb98a2d30e7f503e2fa37
suitangi <suitangi777@gmail.com>
2016-08-27 17:55:31 -0700

    Might break

--- packages/nova-base-components/lib/common/Header.jsx
+++ packages/nova-base-components/lib/common/Header.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 //import { Messages } from "meteor/nova:core";
 
 const Header = ({currentUser}) => {
-  
+
   const logoUrl = Telescope.settings.get("logoUrl");
   const siteTitle = Telescope.settings.get("title", "Nova");
   const tagline = Telescope.settings.get("tagline");
 
   return (
+    var bodyStyle = {
+      fontFamily: "century gothic",
+    };
     <div className="header-wrapper">
 
-      <header className="header">
+      <header className="header;" style = {headerStyle}>
 
         <div className="logo">
           <Telescope.components.Logo logoUrl={logoUrl} siteTitle={siteTitle} />
           {tagline ? <h2 className="tagline">{tagline}</h2> : "" }
         </div>
-        
+
         <div className="nav">
-          
+
           <div className="nav-user">
             {currentUser ? <Telescope.components.UsersMenu user={currentUser}/> : <Telescope.components.UsersAccountMenu/>}
           </div>
