4919e59ca6941178c619d79f8158cad4c4703ece
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 23:32:08 -0700

    try absolute positioning

--- packages/ninjapenguin-common/lib/components/CustomPostsItem.jsx
+++ packages/ninjapenguin-common/lib/components/CustomPostsItem.jsx
@@ -32,6 +32,7 @@ class CustomPostsItem extends Telescope.components.PostsItem {
 
           <div className="posts-item-content">
             
+            <div className="posts-wrapper">
             <div className="posts-top">
             <h3 className="posts-item-title">
               <Link to={Posts.getLink(post)} className="posts-item-title-link" target={Posts.getLinkTarget(post)}>
@@ -55,6 +56,7 @@ class CustomPostsItem extends Telescope.components.PostsItem {
               {(this.context.currentUser && this.context.currentUser.isAdmin) ?<Telescope.components.PostsStats post={post} />:null}
               {this.renderActions()}
               {this.renderCommenters()}
+            </div>
             </div>
             </div>
 
