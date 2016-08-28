d544683cb30b5aec6190a8cf27136e1ff91c00f9
huaiyudavid <davidyangrocs@gmail.com>
2016-08-27 23:58:32 -0700

    offset col

--- packages/nova-base-components/lib/posts/PostsLoadMore.jsx
+++ packages/nova-base-components/lib/posts/PostsLoadMore.jsx
@@ -3,8 +3,8 @@ import { FormattedMessage } from 'react-intl';
 
 const PostsLoadMore = ({loadMore, count, totalCount}) => {
   return (
-    <div className="col-md-6 posts-load-more-wrapper">
-        <a className="posts-load-more row" onClick={loadMore}>
+    <div className="col-md-6 col-md-offset-3 posts-load-more-wrapper">
+        <a className="posts-load-more" onClick={loadMore}>
           <span><FormattedMessage id="posts.load_more"/></span>
           &nbsp;
           {totalCount ? <span className="load-more-count">{`(${count}/${totalCount})`}</span> : null}
